77d9b2b3911ba9e1953350f054a466e384fde959
SENK001 <15095810610@163.com>
2024-09-26 01:03:58 +0800

    修改关于对话框

--- MouseHook/framework.h
+++ MouseHook/framework.h
@@ -16,3 +16,9 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
+// 使用现代控件风格   
+#pragma comment(linker,"\"/manifestdependency:type='win32' "\
+						"name='Microsoft.Windows.Common-Controls' "\
+						"version='6.0.0.0' processorArchitecture='*' "\
+						"publicKeyToken='6595b64144ccf1df' language='*'\"")
